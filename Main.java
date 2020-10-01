02669fe47ed10cddf7712acf76759d6233618652
balez4464 <balez4464@wrdsb.ca>
2020-10-01 15:47:10 +0000

    Made a program that gives user their maxium number

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ *Program gives user his maximum number
+ * @author Zachary Balean
  */
 public class Main {
 
@@ -9,7 +11,23 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // creates Scanner
+    Scanner input = new Scanner(System.in);
+
+    //asks and allows user to input their numbers
+    System.out.println("Please enter three integers on separate lines");
+    int firstNumber = input.nextInt();
+    int secondNumber = input.nextInt();
+    int thirdNumber = input.nextInt();   
+
+    //finds maximum number
+    if(firstNumber >= secondNumber && firstNumber >= thirdNumber){
+      System.out.println("Maximum: " + firstNumber);
+    } else if(secondNumber >= firstNumber && secondNumber >= thirdNumber){
+      System.out.println("Maximum: " + secondNumber);
+    }else{
+      System.out.println("Maximum: " + thirdNumber);
+    }
+
   }
 }
